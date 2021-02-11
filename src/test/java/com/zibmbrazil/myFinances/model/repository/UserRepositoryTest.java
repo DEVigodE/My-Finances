95bf3843edde9e8c7a67980a618dabf350913907
ZIBMBRAZIL <moreira_igor9@hotmail.com>
2021-02-11 11:44:58 -0300

    testing improvements
    Refactoring the User test
    
    Refatorando o teste de Usuário

--- src/test/java/com/zibmbrazil/myFinances/model/repository/UserRepositoryTest.java
+++ src/test/java/com/zibmbrazil/myFinances/model/repository/UserRepositoryTest.java
@@ -4,25 +4,34 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import com.zibmbrazil.myFinances.model.entity.User;
 
-@SpringBootTest
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 public class UserRepositoryTest {
 
 	@Autowired
 	UserRepository repository;
+	
+	@Autowired
+	TestEntityManager entityManager;
 
 	@Test
 	public void shouldCheckForAnEmail() {
 
 		// SCENARIO
 		User user = User.builder().name("Igor").email("igor@email.com").build();
-		repository.save(user);
+		entityManager.persist(user);
 
 		// ACTION / EXECUTE
 		boolean result = repository.existsByEmail("igor@email.com");
@@ -33,7 +42,7 @@ public class UserRepositoryTest {
 
 	@Test
 	public void iMustReturnFalseWhenThereIsNoUserRegisteredWithTheEmail() {
-		repository.deleteAll();
+
 
 		// ACTION / EXECUTE
 		boolean result = repository.existsByEmail("igor@email.com");
